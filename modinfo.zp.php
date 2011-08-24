a141e98159dfa0733296489f091ec939bd845fb1
Bobby Allen <bobbyallen.uk@gmail.com>
2011-08-24 10:31:16 +0100

    Adding all files to be added to the initial commit.

---
+++ modinfo.zp.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* ZANTASTICO
+* Zantastico is a free module developed by Bobby Allen to deploy open-source web scripts to user hosting accounts on ZPanel.
+*/
+
+$thismod['title'] = 'Zantastico';
+$thismod['icon'] = 'zantastico.gif';
+$thismod['developer'] = 'Bobby Allen (ballen@zpanel.co.uk)';
+$thismod['developersite'] = 'http://www.ballen.co.uk/';
+?>
